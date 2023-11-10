a35d87472a7913b13720159944d76152812ad2dd
Tyler Aycock <tyler.joseph.aycock@gmail.com>
2023-11-10 13:21:09 -0800

    initial set up for hamburger menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./Header.css";
 import avatar from "../assets/images/image-avatar.png";
 import { AiOutlineShoppingCart } from "react-icons/ai";
+import Hamburger from "./mobileNav/Hamburger";
 
 const Header = ({ modal, setModal, quantity, setQuantity }) => {
   const clickHandler = () => {
@@ -11,8 +12,9 @@ const Header = ({ modal, setModal, quantity, setQuantity }) => {
   return (
     <header className="header__container">
       <div className="left-side__container">
+        <Hamburger />
         <h1>sneakers</h1>
-        <nav>
+        <nav className="navbar">
           <a href="#">Collections</a>
           <a href="#">Men</a>
           <a href="#">Women</a>
@@ -22,10 +24,11 @@ const Header = ({ modal, setModal, quantity, setQuantity }) => {
       </div>
       <div className="right-side__container">
         <div className="cart__container">
-        <div className="cart__count">
-          {quantity>0 && quantity}
-        </div>
-        <AiOutlineShoppingCart className="shopping-cart" onClick={clickHandler}/>
+          <div className="cart__count">{quantity > 0 && quantity}</div>
+          <AiOutlineShoppingCart
+            className="shopping-cart"
+            onClick={clickHandler}
+          />
         </div>
         <img
           src={avatar}
